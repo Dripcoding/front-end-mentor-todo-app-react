0d752045887e9130b8352a5101b609a5be464b33
Dripcoding <dripcoding@gmail.com>
2021-03-02 22:01:50 -0800

    set up snapshot test for App

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from '../containers/App/App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe("App", () => {
+    it("matches snapshot", () => {
+        const { container } = render(<App />);
+        expect(container).toMatchSnapshot();
+    })
+})
