a10ba2da92ad462aedae3be6ab023c7458f45ed7
Dripcoding <dripcoding@gmail.com>
2021-03-04 20:18:15 -0800

    set up todo header and theme toggle icon

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -1,8 +1,17 @@
+import {ReactComponent as SunToggleImg} from '../../assets/images/icon-sun.svg';
+import MoonToggleImg from '../../assets/images/icon-moon.svg';
+
 import './App.scss';
 
-function App() {
+const STYLE_BASE: string = 'appContainer__';
+
+const App = (): JSX.Element => {
   return (
-    <main className={`appContainer background-desktop-dark`}>
+    <main className={`${STYLE_BASE}mainContainer background-desktop-dark`}>
+        <div className={`${STYLE_BASE}titleContainer`}>
+            <h1>Todo</h1>
+            <SunToggleImg />
+        </div>
     </main>
   );
 }
