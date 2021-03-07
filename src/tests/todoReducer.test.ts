6c2671ab6e886cabd429649f8654fc5e8ac23858
Dripcoding <dripcoding@gmail.com>
2021-03-06 18:03:27 -0800

    add test to delete todo

--- src/tests/todoReducer.test.ts
+++ src/tests/todoReducer.test.ts
@@ -1,11 +1,12 @@
-import { reducer, CREATE_TODO, EDIT_TODO } from '../context/reducer'
+import { notEqual } from 'node:assert'
+import { reducer, CREATE_TODO, EDIT_TODO, DELETE_TODO } from '../context/reducer'
 
 describe("todo reducer" , () => {
 
     it("creates todo", () => {
         const action = { type: CREATE_TODO, payload: { id: 2, text: "second todo"}}
         const state = {
-            todos: [{id: 1, text: "first todo"}]
+            todos: [{ id: 1, text: "first todo" }]
         }
         const result = reducer(state, action);
         const expectedTodos = [...state.todos, action.payload];
@@ -17,12 +18,24 @@ describe("todo reducer" , () => {
     it("edits todo", () => {
         const action = { type: EDIT_TODO, payload: {id: 2, text: "new todo text"}}
         const state = {
-            todos: [{id: 1, text: "first todo"}, {id: 2, text: "second todo"}]
+            todos: [{ id: 1, text: "first todo" }, { id: 2, text: "second todo" }]
         }
         const result = reducer(state, action);
         const expectedTodos = state.todos
 
         expect(result.todos).not.toBeNull();
         expect(result.todos).toEqual(expectedTodos);
+    })
+
+    it("delete todo", () => {
+        const action = { type: DELETE_TODO, payload: { id: 1, text: "todo to delete" }}
+        const state = {
+            todos: [{ id: 1, text: "first todo" }, { id: 2, text: "second todo" }]
+        }
+        const result = reducer(state, action);
+        const expectedTodos = [state.todos[1]];
+
+        expect(result.todos).not.toBeNull();
+        expect(result.todos).toEqual(expectedTodos)
     })
 })
