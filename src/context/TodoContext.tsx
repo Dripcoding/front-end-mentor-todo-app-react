6582df78dae0d2afbd5f08914976393adec6466f
Dripcoding <dripcoding@gmail.com>
2021-03-06 17:17:53 -0800

    set up useReducer hook in todoContext

--- src/context/TodoContext.tsx
+++ src/context/TodoContext.tsx
@@ -1,19 +1,27 @@
-import React from 'react';
+import React, { useReducer } from 'react';
+import { reducer } from './reducer';
 
 interface ITodoContextProps {
     children: React.ReactChild
 }
 
-const initialState = {
-    todos: []
+export interface ITodo {
+    id: number;
+    text: string;
 }
 
-const TodoContext = React.createContext({
-    todos: []
-});
+export interface ITodos {
+    todos: ITodo[];
+}
+
+const initialState: ITodos = {
+    todos: [{id: 1, text: "hello"}]
+}
+
+const TodoContext = React.createContext(initialState);
 
 const TodoContextProvider = ({children}: ITodoContextProps): JSX.Element => {
- 
+    const [state, dispatch] = useReducer(reducer, initialState);
 
     return (
         <TodoContext.Provider value={initialState}>
