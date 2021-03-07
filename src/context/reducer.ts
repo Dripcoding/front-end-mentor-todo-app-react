6d3b4d718aa569876745dbc1b3ae667b9803bb32
Dripcoding <dripcoding@gmail.com>
2021-03-06 17:16:44 -0800

    set up reducer func with CREATE_TODO action type

--- src/context/reducer.ts
+++ src/context/reducer.ts
@@ -1,8 +1,24 @@
+import { ITodo, ITodos } from './TodoContext';
+
 // todo action types
 export const CREATE_TODO: string = 'CREATE_TODO';
 export const EDIT_TODO: string = 'EDIT_TODO';
 export const DELETE_TODO: string = 'DELETE_TODO';
 
+interface IAction {
+    type: string;
+    payload: ITodo 
+}
 
+// todo reducer
+export const reducer = (state: ITodos, action: IAction) => {
+    switch(action.type) {
+        case CREATE_TODO:
+            return { todos: [...state.todos, action.payload] }     
+        // case EDIT_TODO:
+        // case DELETE_TODO:
 
-
+        default:
+            return state;
+    }
+}
