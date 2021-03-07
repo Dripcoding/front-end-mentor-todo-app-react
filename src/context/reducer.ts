5cea7d24fdfd953b904d788083742867e23df6f2
Dripcoding <dripcoding@gmail.com>
2021-03-06 21:55:04 -0800

    add additional action types

--- src/context/reducer.ts
+++ src/context/reducer.ts
@@ -1,9 +1,15 @@
 import { ITodo, ITodos } from './TodoContext';
 
-// todo action types
 export const CREATE_TODO: string = 'CREATE_TODO';
 export const EDIT_TODO: string = 'EDIT_TODO';
 export const DELETE_TODO: string = 'DELETE_TODO';
+export const COMPLETE_TODO: string = 'COMPLETE_TODO';
+export const ACTIVATE_TODO: string = 'ACTIVATE_TODO';
+export const DELETE_ALL_TODOS: string = 'DELETE_ALL_TODOS';
+export const VIEW_ALL_TODOS: string = 'VIEW_ALL_TODOS';
+export const VIEW_ALL_COMPLETED_TODOS: string = 'VIEW_ALL_COMPLETED_TODOS';
+export const VIEW_ALL_ACTIVE_TODOS: string = 'VIEW_ALL_ACTIVE_TODOS';
+
 
 interface IAction {
     type: string;
