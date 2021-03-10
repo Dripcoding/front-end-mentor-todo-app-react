f7ad9723373e41db7a3d193ca1b15011c83c57c1
Dripcoding <dripcoding@gmail.com>
2021-03-09 21:35:57 -0800

    add prettier dev dep, reformat

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,21 +1,21 @@
-import { render } from '@testing-library/react';
-import App from '../containers/App/App';
+import { render } from "@testing-library/react";
+import App from "../containers/App/App";
 
 describe("App", () => {
-    it("matches snapshot", () => {
-        const { container } = render(<App />);
-        expect(container).toMatchSnapshot();
-    })
+  it("matches snapshot", () => {
+    const { container } = render(<App />);
+    expect(container).toMatchSnapshot();
+  });
 
-    it("renders correctly", () => {
-        const { getByRole, getByTestId } = render(<App />);
+  it("renders correctly", () => {
+    const { getByRole, getByTestId } = render(<App />);
 
-        const todoHeader = getByRole('heading');
-        expect(todoHeader).toBeInTheDocument();
-        expect(todoHeader).toBeVisible()
-        
-        const sunIcon = getByTestId('SUN-TOGGLE-ICON');
-        expect(sunIcon).toBeInTheDocument();
-        expect(sunIcon).toBeVisible();
-    })
-})
+    const todoHeader = getByRole("heading");
+    expect(todoHeader).toBeInTheDocument();
+    expect(todoHeader).toBeVisible();
+
+    const sunIcon = getByTestId("SUN-TOGGLE-ICON");
+    expect(sunIcon).toBeInTheDocument();
+    expect(sunIcon).toBeVisible();
+  });
+});
--- src/tests/todoReducer.test.ts
+++ src/tests/todoReducer.test.ts
@@ -1,149 +1,189 @@
 import {
-    ACTIVATE_TODO,
-    COMPLETE_TODO,
-    CREATE_TODO,
-    DELETE_ALL_TODOS,
-    DELETE_TODO,
-    EDIT_TODO,
-    todoReducer,
-    VIEW_ALL_ACTIVE_TODOS,
-    VIEW_ALL_COMPLETED_TODOS,
-    VIEW_ALL_TODOS
-} from '../context/reducer'
+  ACTIVATE_TODO,
+  COMPLETE_TODO,
+  CREATE_TODO,
+  DELETE_ALL_TODOS,
+  DELETE_TODO,
+  EDIT_TODO,
+  todoReducer,
+  VIEW_ALL_ACTIVE_TODOS,
+  VIEW_ALL_COMPLETED_TODOS,
+  VIEW_ALL_TODOS,
+} from "../context/reducer";
 
-describe("todo reducer" , () => {
+describe("todo reducer", () => {
+  it("creates todo", () => {
+    const action = {
+      type: CREATE_TODO,
+      payload: { id: 2, text: "second todo", active: true },
+    };
+    const state = {
+      todos: [{ id: 1, text: "first todo", active: true }],
+    };
+    const result = todoReducer(state, action);
+    const expectedTodos = [...state.todos, action.payload];
 
-    it("creates todo", () => {
-        const action = { type: CREATE_TODO, payload: { id: 2, text: "second todo", active: true}}
-        const state = {
-            todos: [{ id: 1, text: "first todo", active: true }]
-        }
-        const result = todoReducer(state, action);
-        const expectedTodos = [...state.todos, action.payload];
-       
-        expect(result.todos).not.toBeNull();
-        expect(result.todos).toEqual(expectedTodos)
-    })
-    it("edits todo", () => {
-        const action = { type: EDIT_TODO, payload: {id: 2, text: "new todo text", active: true}}
-        const state = {
-            todos: [{ id: 1, text: "first todo", active: true }, { id: 2, text: "second todo", active: true }]
-        }
-        const result = todoReducer(state, action);
-        const expectedTodos = state.todos
+    expect(result.todos).not.toBeNull();
+    expect(result.todos).toEqual(expectedTodos);
+  });
+  it("edits todo", () => {
+    const action = {
+      type: EDIT_TODO,
+      payload: { id: 2, text: "new todo text", active: true },
+    };
+    const state = {
+      todos: [
+        { id: 1, text: "first todo", active: true },
+        { id: 2, text: "second todo", active: true },
+      ],
+    };
+    const result = todoReducer(state, action);
+    const expectedTodos = state.todos;
 
-        expect(result.todos).not.toBeNull();
-        expect(result.todos).toEqual(expectedTodos);
-    })
+    expect(result.todos).not.toBeNull();
+    expect(result.todos).toEqual(expectedTodos);
+  });
 
-    it("deletes todo", () => {
-        const action = { type: DELETE_TODO, payload: { id: 1, text: "todo to delete", active: true }}
-        const state = {
-            todos: [{ id: 1, text: "first todo", active: true }, { id: 2, text: "second todo", active: true }]
-        }
-        const result = todoReducer(state, action);
-        const expectedTodos = [state.todos[1]];
+  it("deletes todo", () => {
+    const action = {
+      type: DELETE_TODO,
+      payload: { id: 1, text: "todo to delete", active: true },
+    };
+    const state = {
+      todos: [
+        { id: 1, text: "first todo", active: true },
+        { id: 2, text: "second todo", active: true },
+      ],
+    };
+    const result = todoReducer(state, action);
+    const expectedTodos = [state.todos[1]];
 
-        expect(result.todos).not.toBeNull();
-        expect(result.todos).toEqual(expectedTodos)
-    })
+    expect(result.todos).not.toBeNull();
+    expect(result.todos).toEqual(expectedTodos);
+  });
 
-    it("returns given state as the default behavior", () => {
-        const DEFAULT_ACTION = "default"
-        const action = { type: DEFAULT_ACTION, payload: { id: 1, text: "default", active: true }}
-        const state = {
-            todos: [{ id: 1, text: "first todo", active: true }, { id: 2, text: "second todo", active: true }]
-        }
-        const result = todoReducer(state, action);
+  it("returns given state as the default behavior", () => {
+    const DEFAULT_ACTION = "default";
+    const action = {
+      type: DEFAULT_ACTION,
+      payload: { id: 1, text: "default", active: true },
+    };
+    const state = {
+      todos: [
+        { id: 1, text: "first todo", active: true },
+        { id: 2, text: "second todo", active: true },
+      ],
+    };
+    const result = todoReducer(state, action);
 
-        expect(result.todos).not.toBeNull();
-        expect(result.todos).toEqual(state.todos)
-    })
+    expect(result.todos).not.toBeNull();
+    expect(result.todos).toEqual(state.todos);
+  });
 
-    it("sets active state to false for a complete todo", () => {
-        const action = { type: COMPLETE_TODO, payload: { id: 1, text: "first todo", active: true}}
-        const state = {
-            todos: [{ id: 1, text: "first todo", active: true}]
-        }
-        const result = todoReducer(state, action);
+  it("sets active state to false for a complete todo", () => {
+    const action = {
+      type: COMPLETE_TODO,
+      payload: { id: 1, text: "first todo", active: true },
+    };
+    const state = {
+      todos: [{ id: 1, text: "first todo", active: true }],
+    };
+    const result = todoReducer(state, action);
 
-        expect(result.todos).not.toBeNull();
-        expect(result.todos).toEqual(state.todos);
-    })
+    expect(result.todos).not.toBeNull();
+    expect(result.todos).toEqual(state.todos);
+  });
 
-    it("sets active state to true for an active todo", () => {
-        const action = {type: ACTIVATE_TODO, payload: {id: 1, text: "example todo", active: false}}
-        const state = {
-            todos: [{id: 1, text: "first todo", active: true}]
-        }
-        const result = todoReducer(state, action)
+  it("sets active state to true for an active todo", () => {
+    const action = {
+      type: ACTIVATE_TODO,
+      payload: { id: 1, text: "example todo", active: false },
+    };
+    const state = {
+      todos: [{ id: 1, text: "first todo", active: true }],
+    };
+    const result = todoReducer(state, action);
 
-        expect(result.todos).not.toBeNull();
-        expect(result.todos).toEqual(state.todos);
-    })
+    expect(result.todos).not.toBeNull();
+    expect(result.todos).toEqual(state.todos);
+  });
 
-    it("deletes all todos", () => {
-        const action = {type: DELETE_ALL_TODOS, payload: {id: 1, text: "", active: false}}
-        const state = {
-            todos: [
-                {id: 1, text: "first todo", active: true},
-                {id: 2, text: "second todo", active: true},
-                {id: 3, text: "third todo", active: false}
-            ]
-        }
-        const result = todoReducer(state, action);
+  it("deletes all todos", () => {
+    const action = {
+      type: DELETE_ALL_TODOS,
+      payload: { id: 1, text: "", active: false },
+    };
+    const state = {
+      todos: [
+        { id: 1, text: "first todo", active: true },
+        { id: 2, text: "second todo", active: true },
+        { id: 3, text: "third todo", active: false },
+      ],
+    };
+    const result = todoReducer(state, action);
 
-        expect(result.todos).not.toBeNull();
-        expect(result.todos.length).toBe(0);
-    })
+    expect(result.todos).not.toBeNull();
+    expect(result.todos.length).toBe(0);
+  });
 
-    it("shows all completed todos", () => {
-        const action = {type: VIEW_ALL_COMPLETED_TODOS, payload: {id: 1, text: "", active: false}}
-        const state = {
-            todos: [
-                {id: 1, text: "first todo", active: true},
-                {id: 2, text: "second todo", active: true},
-                {id: 3, text: "third todo", active: false}
-            ]
-        }
-        const expected = [{id: 3, text: "third todo", active: false}]
-        const result = todoReducer(state, action)
+  it("shows all completed todos", () => {
+    const action = {
+      type: VIEW_ALL_COMPLETED_TODOS,
+      payload: { id: 1, text: "", active: false },
+    };
+    const state = {
+      todos: [
+        { id: 1, text: "first todo", active: true },
+        { id: 2, text: "second todo", active: true },
+        { id: 3, text: "third todo", active: false },
+      ],
+    };
+    const expected = [{ id: 3, text: "third todo", active: false }];
+    const result = todoReducer(state, action);
 
-        expect(result.todos).not.toBeNull();
-        expect(result.todos.length).toBe(1);
-        expect(result.todos).toEqual(expected)
-    })
+    expect(result.todos).not.toBeNull();
+    expect(result.todos.length).toBe(1);
+    expect(result.todos).toEqual(expected);
+  });
 
-    it("shows all active todos", () => {
-        const action = {type: VIEW_ALL_ACTIVE_TODOS, payload: {id: 1, text: "", active: false}}
-        const state = {
-            todos: [
-                {id: 1, text: "first todo", active: true},
-                {id: 2, text: "second todo", active: true},
-                {id: 3, text: "third todo", active: false}
-            ]
-        }
-        const expected = [{id: 1, text: "first todo", active: true}, {id: 2, text: "second todo", active: true}]
-        const result = todoReducer(state, action)
+  it("shows all active todos", () => {
+    const action = {
+      type: VIEW_ALL_ACTIVE_TODOS,
+      payload: { id: 1, text: "", active: false },
+    };
+    const state = {
+      todos: [
+        { id: 1, text: "first todo", active: true },
+        { id: 2, text: "second todo", active: true },
+        { id: 3, text: "third todo", active: false },
+      ],
+    };
+    const expected = [
+      { id: 1, text: "first todo", active: true },
+      { id: 2, text: "second todo", active: true },
+    ];
+    const result = todoReducer(state, action);
 
-        expect(result.todos).not.toBeNull()
-        expect(result.todos.length).toBe(2)
-        expect(result.todos).toEqual(expected)
-    })
+    expect(result.todos).not.toBeNull();
+    expect(result.todos.length).toBe(2);
+    expect(result.todos).toEqual(expected);
+  });
 
-    it("shows all todos", () => {
-        const action = {type: VIEW_ALL_TODOS, payload: {id: 1, text: "", active: false}}
-        const state = {
-            todos: [
-                {id: 1, text: "first todo", active: true},
-                {id: 2, text: "second todo", active: true},
-                {id: 3, text: "third todo", active: false}
-            ]
-        }
-        const result = todoReducer(state, action)
+  it("shows all todos", () => {
+    const action = {
+      type: VIEW_ALL_TODOS,
+      payload: { id: 1, text: "", active: false },
+    };
+    const state = {
+      todos: [
+        { id: 1, text: "first todo", active: true },
+        { id: 2, text: "second todo", active: true },
+        { id: 3, text: "third todo", active: false },
+      ],
+    };
+    const result = todoReducer(state, action);
 
-        expect(result.todos).not.toBeNull()
-        expect(result.todos).toEqual(state.todos)
-    })
-
-})
+    expect(result.todos).not.toBeNull();
+    expect(result.todos).toEqual(state.todos);
+  });
+});
