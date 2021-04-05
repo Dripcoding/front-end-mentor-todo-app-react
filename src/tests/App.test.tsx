fc180d616b9147e5df0c2779d4000f15a5fa34ed
Dripcoding <dripcoding@gmail.com>
2021-04-04 17:40:22 -0700

    create Title component

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -6,16 +6,4 @@ describe("App", () => {
     const { container } = render(<App />);
     expect(container).toMatchSnapshot();
   });
-
-  it("renders correctly", () => {
-    const { getByRole, getByTestId } = render(<App />);
-
-    const todoHeader = getByRole("heading");
-    expect(todoHeader).toBeInTheDocument();
-    expect(todoHeader).toBeVisible();
-
-    const sunIcon = getByTestId("SUN-TOGGLE-ICON");
-    expect(sunIcon).toBeInTheDocument();
-    expect(sunIcon).toBeVisible();
-  });
 });
